23ace9581c6b3bb8ce9dae4e5d3f1102e31c0713
Raphaël Lemaire <lemaire.raphael@gmail.com>
2016-05-17 21:33:31 +0200

    Week 6

--- snippets/src/Product.java
+++ snippets/src/Product.java
@@ -3,7 +3,17 @@
  */
 public class Product {
     public static void main(String[] args) {
-        double err = 0.49;
-        System.out.println(0.5 * Math.log((1 - err) / err));
+        double nbFalseNegatives = 40;
+        double nbFalsePositives = 1900;
+        double nbTruePositives = 5600;
+        double nbTrueNegatives = 2460;
+        double precision = nbTruePositives / (nbTruePositives + nbFalsePositives);
+        double recall = nbTruePositives / (nbTruePositives + nbFalseNegatives);
+        double erreur = (nbFalseNegatives + nbFalsePositives) / (nbTruePositives + nbTrueNegatives +  nbFalseNegatives + nbFalsePositives);
+        System.out.println("Précision : " + precision);
+        System.out.println("Recall : " + recall);
+        System.out.println("Erreur : " + erreur);
+        System.out.println("Nb non pos : " + (nbTrueNegatives + nbFalseNegatives + nbTrueNegatives));
+        System.out.println("Lowest score : " + 1 / (1 + Math.exp(-0.9)));
     }
 }
